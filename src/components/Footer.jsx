2195a8be512fd836d37b1ee6f3db602062f08356
jayaadeep11 <jayadeep.bellamkonda@gmail.com>
2024-05-21 19:03:00 +0530

    edit contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,11 @@ const Footer = () => {
   return (
     <div className=" Contact h-[80vh] flex flex-col justify-between  hello lg:px-24 px-16  mx-auto sticky bottom-0  z-0 ">
       <div className="flex flex-wrap">
-        <div className="lg:text-9xl  lg:w-2/3 text-3xl pt-10   lg:pt-36  w-full  font-medium text-black tracking-[-0.02em]	">
-          <span className="">Let's Work</span> Together<PiArrowSquareIn className="opacity-0 lg:opacity-100  inline text-white" /></div>
+        <div className="lg:text-9xl    lg:w-2/3 text-3xl pt-10   lg:pt-36  w-full  font-medium text-black tracking-[-0.02em]	">
+          <a href="mailto:jayadeep.bellamkonda@gmail.com" className="group">
+            <span className="">Let's Work</span> Together<span><PiArrowSquareIn className="group-hover:scale-75 transition-all  inline text-white" /></span>
+          </a>
+        </div>
 
         <div className=" pt-36 flex justify-end  w-full lg:w-1/3">
           <ul className="text-black text-4xl flex flex-col gap-5 items-center justify-center">
