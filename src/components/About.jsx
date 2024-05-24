f0421a793d599e9dadee19debd358e80ef64bbf6
Jayaadeep11 <jayadeep.bellamkonda@gmail.com>
2024-05-24 16:03:52 +0530

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,38 @@
+import logo from "./../assets/profilepic.png";
+
 
 const About = () => {
   return (
-    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
-      <div className="text-center">
-        <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
-          About
+    <div className="flex flex-col justify-center items-center mt-20 border-b border-neutral-800 min-h-[600px]">
+
+      <h2 className="text-center text-5xl lg:text-8xl font-bold  arch">
+        Jayadeep{" "}
+        <span className="">
+          Bellamkonda
         </span>
-        <h2 className="text-3xl sm:text-5xl lg:text-[80px] mt-10 lg:mt-20 tracking-wide">
-          Jayadeep{" "}
-          <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-            Bellamkonda
-          </span>
-        </h2>
+      </h2>
+
+      <div className="flex flex-col lg:flex-row md:flex-row justify-center items-center lg:px-20 md:mx-0  my-10 sm:flex-row lg:mx-32">
+        <div className="lg:w-1/2 ">
+          <img src={logo} className="w-full lg:w-2/3   border border-transparent  rounded-full" />
+        </div>
+        <div className=" flex  flex-col gap-10 lg:text-5xl   lg:w-1/2 mt-10  text-center ">
+          <p className="arch bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+            I'm a Product Designer working remotely from 11°C Dublin, Ireland
+          </p>
+          <div className="flex flex-col gap-10 text-sm lg:text-xl">
+            <p>
+          Over the past 12+ years, Ive worked in various areas of digital design, including front-end development, email, marketing, and app UI/UX. Im proud to have worn many hats.>
+            </p>
+            <p>
+              These days, I focus on leading design at GiveDirectly, a nonprofit that lets donors send money directly to the world's poorest households.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center  flex-wrap mt-10 lg:mt-20">
-        <p className="text-center">Work in progress</p>
-      </div>
-    </div>
+    </div >
   );
 };
 
 export default About;
+
